1765c43806f69cc710eac3009f911291b58cdfe4
杜健德 <dujiande@bluemoon.com.cn>
2017-03-29 17:30:23 +0800

    添加观察者模式

--- app/src/main/java/me/djd/designmode/ui/ObServableActivity.java
+++ app/src/main/java/me/djd/designmode/ui/ObServableActivity.java
@@ -6,9 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import me.djd.designmode.R;
@@ -16,6 +16,7 @@ import me.djd.designmode.base.BaseActivity;
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action1;
+import rx.functions.Func0;
 
 /**
  * Created by dujiande on 2017/3/29.
@@ -29,6 +30,7 @@ public class ObServableActivity extends BaseActivity {
     TextView resultTv;
 
     StringBuilder sb;
+    Observable<Long> myObservable;
 
     public static void appJump(Context context) {
         Intent intent = new Intent(context, ObServableActivity.class);
@@ -98,12 +100,52 @@ public class ObServableActivity extends BaseActivity {
         });
     }
 
+    /**
+     * 4.使用defer( )，有观察者订阅时才创建Observable，并且为每个观察者创建一个新的Observable
+     */
+    private void actionDefer(){
+        Observable<String> deferObservable = Observable.defer(new Func0<Observable<String>>() {
+            @Override
+            public Observable<String> call() {
+                return Observable.just("one");
+            }
+        });
+
+        deferObservable.subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                sb.append(s+"\n");
+                resultTv.setText(sb.toString());
+            }
+        });
+    }
+
+    /**
+     * 5.使用interval( ),创建一个按固定时间间隔发射整数序列的Observable，可用作定时器
+     */
+    private void actionInterval(){
+        myObservable = Observable.interval(1, TimeUnit.SECONDS);
+        myObservable.subscribe(new Action1<Long>() {
+            @Override
+            public void call(Long aLong) {
+                sb.append(aLong+"\n");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultTv.setText(sb.toString());
+                    }
+                });
+
+            }
+        });
+    }
+
     @Override
     public void initListener() {
         observableBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                actionFrom();
+                actionInterval();
             }
         });
     }
